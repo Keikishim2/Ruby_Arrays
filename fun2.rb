afc008fc7144d31ec80a460944a839a3e23c8e90
Keikishim2 <Keikishim2@gmail.com>
2021-06-21 19:36:08 +0800

    first

---
+++ fun2.rb
@@ -0,0 +1,16 @@
+a = ["Matz", "Kei", "Zeus", "Fitz", "Mart"]
+b = [1, 2, 3, 4, 5, 6]
+c = a+b
+puts a[0]
+puts a[1]
+puts b.class
+puts b.shuffle.join("-")
+puts c.to_s
+puts a.reverse
+puts c.join(' and ')
+
+{}.empty?
+
+[].empty?
+
+"".empty?
